ed6012bc35a1d9852d16d7a39f2bb4a71f9e4788
Thomas Roberts <robertsorange@gmail.com>
2019-11-10 15:08:23 -0700

    Added size tests

--- App.tsx
+++ App.tsx
@@ -1,21 +1,30 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Button } from 'react-native';
 import Bananas from  './Bananas';
 import Greetings from './Greetings';
 import Blink from "./Blink";
 import LotsOfStyles from "./Styled";
+import FixedDimensions from './FixedDimensions';
+import FlexDimensions from './FlexDimensions';
+
 
 export default class HelloWorldApp extends Component {
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Hello, world!</Text>
-        <Bananas />
-        <Greetings name='Thom' />
-        <Greetings name='Bennie' />
-        <Blink text={'I\'m a piece of flashing text'} />
-        <LotsOfStyles />
+      <View style={{flex: 1}}>
+        <FlexDimensions />
       </View>
+        /*
+        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+          <Text>Hello, world!</Text>
+          <Bananas />
+          <Greetings name='Thom' />
+          <Greetings name='Bennie' />
+          <Blink text={'I\'m a piece of flashing text'} />
+          <LotsOfStyles />
+          <FixedDimensions />
+        </View>
+        */
     );
   }
 }
